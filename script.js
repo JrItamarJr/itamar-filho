9a029a6f0296cdf8a94f7e2af1337e7b347c7de9
Itamar Filho <valvassoriitamar@gmail.com>
2024-10-29 14:16:18 -0300

    Update

--- script.js
+++
@@ -1,9 +0,0 @@
-// Adicione um efeito hover aos projetos
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('mouseover', () => {
-        card.style.transform = 'scale(1.05)';
-    });
-    card.addEventListener('mouseout', () => {
-        card.style.transform = 'scale(1)';
-    });
-});
